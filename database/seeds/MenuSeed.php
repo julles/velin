75a424948e242f4774f987e000d36b9c6b2393f1
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-05-15 02:56:32 +0700

    add comment in menu seed

--- database/seeds/MenuSeed.php
+++ database/seeds/MenuSeed.php
@@ -1,14 +1,44 @@
 <?php
+/**
+ * Example :
+ *
+ * Generate Parent Menu:
+ *     \Velin::addMenu([
+            'parent_id'     => null,
+            'title'         => 'Development',
+            'order'         => 100,
+            'controller'    => '#',
+            'slug'          => 'development',
+        ],[]);
+
+    Generate Child Menu : 
+        \Velin::addMenu([
+                'parent_id'     => 'development',
+                'title'         => 'Action',
+                'order'         => 1,
+                'controller'    => 'ActionController',
+                'slug'          => 'action',
+            ],['index','update','delete','create']);
+
+    Updating Menu : 
+
+        \Velin::updateMenu([
+                'parent_id'     => 'development',
+                'title'         => 'Action',
+                'order'         => 1,
+                'controller'    => 'ActionController',
+                'slug'          => 'action',
+            ],['index','update','delete','create']);
+
+    Delete Menu : 
+        \Velin::deleteMenu('slug');
+ */
+
 
 use Illuminate\Database\Seeder;
 
 class MenuSeed extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
     	\Velin::addMenu([
